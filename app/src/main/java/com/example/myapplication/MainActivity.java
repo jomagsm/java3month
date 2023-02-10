557671cebfc662eeec4efc144f1600aa48e1fe5b
JomaGsm <jomagsm@gmail.com>
2023-02-10 13:39:31 +0600

    fix sent email

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -33,10 +32,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sentEmail() {
-        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + emailTV.getText()));
-        intent.setData(Uri.parse("mailto:" + emailTV.getText()));
-        intent.putExtra(Intent.EXTRA_SUBJECT, themeTV.getText());
-        intent.putExtra(Intent.EXTRA_TEXT, textTV.getText());
-        startActivity(Intent.createChooser(intent, "Choose email"));
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{emailTV.getText().toString()});
+        email.putExtra(Intent.EXTRA_SUBJECT, themeTV.getText().toString());
+        email.putExtra(Intent.EXTRA_TEXT, textTV.getText().toString());
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
     }
 }
